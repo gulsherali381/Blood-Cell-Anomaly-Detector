49aa32d9489818c19d7105bbc8e036f0e1c8fa17
Gulsher Ali <gulsherali18849@gmail.com>
2026-09-03 12:20:08 +0500

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -7,11 +7,15 @@ image = (
 
 app = modal.App("blood-cell-anomaly-detector")
 
+# Backend folder se files ko /root mein add karein
 image = image.add_local_file("backend/blood_cell_anomaly_detection.csv", "/root/blood_cell_anomaly_detection.csv")
 image = image.add_local_file("backend/Blood-Cell-Anomaly-Detection.pkl", "/root/Blood-Cell-Anomaly-Detection.pkl")
 image = image.add_local_file("backend/Blood-Cell-Anomaly-Detection-Scaler.pkl", "/root/Blood-Cell-Anomaly-Detection-Scaler.pkl")
 image = image.add_local_file("backend/Blood-Cell-Anomaly-Detection-Encoded.pkl", "/root/Blood-Cell-Anomaly-Detection-Encoded.pkl")
 image = image.add_local_file("backend/main.py", "/root/main.py")
+
+# Naya — static folder (HTML/CSS/JS) ko bhi add karo
+image = image.add_local_dir("backend/static", "/root/static")
 
 @app.function(image=image)
 @modal.asgi_app()
